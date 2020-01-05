f5fd65711fcb316b18790f261dd67fbac71fabf5
Kelvan Ince <incekelvan@gmail.com>
2020-01-05 17:53:02 -0500

    Add endpoint to fetch recipe data from

---
+++ Meals/src/js/index.js
@@ -0,0 +1,14 @@
+// `https://forkify-api.herokuapp.com/api/search?&q=${this.query}
+import axios from 'axios';
+
+async function getResults(query) {
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log('RES!!!', recipes);
+    } catch (err) {
+        alert(err)
+    }
+}
+
+getResults('pizza');
