0203d4e266e1b8c1c1a9dad54c61c1716c626d56
Kelvan Ince <incekelvan@gmail.com>
2020-01-09 10:47:21 -0500

    Add function to retrieve query results

--- Meals/src/js/index.js
+++ Meals/src/js/index.js
@@ -1,5 +1,21 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+const state = {};
+
+const controlSearch = async () => {
+    // Get query from view
+    const query = 'pizza';
+
+    if (query) {
+        state.search = new Search(query);
+
+        await state.search.getResults();
+
+        console.log('STATE', state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
--- Meals/src/js/models/Search.js
+++ Meals/src/js/models/Search.js
@@ -9,7 +9,6 @@ export default class Search {
         try {
             const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
             this.result = res.data.recipes;
-            console.log('RES!!!', this.result);
         } catch (err) {
             alert(err);
         }
