3ad9830f05777a6cbf2d0fa9cea7a76d5129e176
Kelvan Ince <incekelvan@gmail.com>
2020-01-09 15:56:46 -0500

    Add limit to the returned results

--- Meals/src/js/views/searchView.js
+++ Meals/src/js/views/searchView.js
@@ -10,6 +10,22 @@ export const clearResults = () => (
     elements.searchResultList.innerHTML = ''
 );
 
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, current) => {
+            if (acc + current.length <= limit) {
+                newTitle.push(current);
+            }
+            return acc + current.length;
+        }, 0);
+
+        return `${newTitle.join(' ')}`
+    }
+
+    return title;
+};
+
 const renderRecipe = recipe => {
     const markup = `
         <li>
@@ -18,7 +34,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
