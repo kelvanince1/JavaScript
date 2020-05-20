c4dc52bda59857487cc13883dc28e42b4228eef4
Kelvan Ince <incekelvan@gmail.com>
2020-05-20 14:14:30 -0400

    Refactor Progress Bar

--- ProgressBar/script.js
+++ ProgressBar/script.js
@@ -1,16 +1,30 @@
 const bar = document.querySelector('.bar');
 
-function fillBar(seconds) {
-    let current = 0;
-    const interval = setInterval(() => {
-        bar.style.width = current + '%';
-        current++;
+// function fillBar(seconds) {
+//     let current = 0;
+//     const interval = setInterval(() => {
+//         bar.style.width = current + '%';
+//         current++;
 
-        console.log('Increased');
+//         console.log('Increased');
 
-        if (current >= 100) {
-            clearInterval(interval);
-        }
+//         if (current >= 100) {
+//             clearInterval(interval);
+//         }
 
-    }, (seconds * 1000) /100);
+//     }, (seconds * 1000) /100);
+// }
+
+let current = 0;
+
+function incrementBar(amount) {
+    current += amount;
+
+    if (current > 100) {
+        bar.style.width = '100%';
+        return;
+    }
+
+    bar.style.width = current  + '%';
+    return;
 }
