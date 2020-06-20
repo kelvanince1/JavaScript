f8bb28b707e2de633ba2af9eda15c9968ac33a51
Kelvan Ince <incekelvan@gmail.com>
2020-06-20 12:27:50 -0400

    Add active list class and storage

--- Todo/script.js
+++ Todo/script.js
@@ -3,7 +3,17 @@ const newListForm = document.querySelector('[data-new-list-form');
 const newListInput = document.querySelector('[data-new-list-input');
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+
 const lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
+listsContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId;
+        saveAndRender();
+    }
+})
 
 newListForm.addEventListener('submit',  e => {
     e.preventDefault();
@@ -25,6 +35,7 @@ function createList(name) {
 
 function save() {
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 }
 
 function saveAndRender() {
@@ -39,6 +50,10 @@ function renderList() {
         listElement.dataset.listId = list.id;
         listElement.classList.add('list-name');
         listElement.textContent = list.name;
+
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list');
+        }
 
         listsContainer.appendChild(listElement);
     })
