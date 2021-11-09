b17ef705d5b60999a586a200fb3f1f0982d8bc82
Kelvan Ince <incekelvan@gmail.com>
2021-11-08 19:42:02 -0800

    Add api call to retrieve total amount raised

---
+++ Ocean/index.js
@@ -0,0 +1,18 @@
+const trashContainer = document.querySelector('.trash-container');
+const moneyElem = document.querySelector('.money');
+const currencyFormatter = new Intl.NumberFormat('en-us', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 0
+});
+
+const amountRaised = 1230243;
+
+async function setupTrash() {
+    const amountRaised = await fetch('https://tscache.com/donation_total.json')
+        .then(res => res.json())
+        .then(data => data.count)
+    
+    moneyElem.innerText = currencyFormatter.format(amountRaised);
+}
+setupTrash();
